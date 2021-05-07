3c93774ca5e03946c92c40dad2b7818bc239a880
emersonfelipesp <emerson.felipe@nmultifibra.com.br>
2021-05-07 17:57:38 -0300

    change version to 0.0.3.dev1 and add init file to all dir

--- setup.py
+++ setup.py
@@ -22,18 +22,28 @@ requires = [
 
 setup(
     name="netbox-proxbox",
-    version="0.0.3",
+    version="0.0.3.dev1",
     author="Emerson Pereira",
     author_email="emerson.felipe@nmultifibra.com.br",
     description="Integration between Proxmox and Netbox",
+    url='https://github.com/N-Multifibra/netbox-proxbox',
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: Linux",
+        "License :: OSI Approved :: Apache Software License",
     ],
-    #package_dir={"": "src"},
-    packages=['netbox_proxbox'],
+    keywords="netbox netbox-plugin plugin proxmox proxmoxer pynetbox",
+    project_urls={
+        'Source': 'https://github.com/N-Multifibra/netbox-proxbox',
+    },
+    packages=find_packages(),
     install_requires=requires,
-    python_requires= '>=3.6'
+    python_requires= '>=3.6',
+    package_data={
+        '': ['*.html'],
+        'netbox_proxbox.api': ['*','*/*','*/*/*'],
+        'netbox_proxbox.templates': ['*','*/*','*/*/*'],
+    },
 )
