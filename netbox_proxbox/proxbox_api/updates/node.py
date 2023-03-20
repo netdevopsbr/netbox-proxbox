6b71aa95d8900037d993c21f53147e0796d79e38
emersonfelipesp <emersonfelipe.2003@gmail.com>
2023-03-20 13:40:24 +0000

    check if 'netbox_node' is None

--- netbox_proxbox/proxbox_api/updates/node.py
+++ netbox_proxbox/proxbox_api/updates/node.py
@@ -61,10 +61,27 @@ def cluster(netbox_node, proxmox_node, proxmox_cluster):
     #
     cluster_updated = False
 
-    try:
-        if proxmox_cluster != None and netbox_node != None: 
-            # If cluster is not filled or even filled, but different from actual cluster, update it.
-            if netbox_node.cluster.name != proxmox_cluster['name'] or netbox_node.cluster.name == None:
+    if netbox_node != None:
+        try:
+            if proxmox_cluster != None and netbox_node != None: 
+                # If cluster is not filled or even filled, but different from actual cluster, update it.
+                if netbox_node.cluster.name != proxmox_cluster['name'] or netbox_node.cluster.name == None:
+                    # Search for Proxmox Cluster using create.cluster() function
+                    cluster_id = create.virtualization.cluster().id
+
+                    # Use Cluster ID to update NODE information
+                    netbox_node.cluster.id = cluster_id
+
+                    if netbox_node.save() == True:
+                        cluster_updated = True
+                    else:
+                        cluster_updated = False
+
+                else:
+                    cluster_updated = False
+
+            # If cluster is empty, update it.
+            elif proxmox_cluster == None:
                 # Search for Proxmox Cluster using create.cluster() function
                 cluster_id = create.virtualization.cluster().id
 
@@ -75,31 +92,16 @@ def cluster(netbox_node, proxmox_node, proxmox_cluster):
                     cluster_updated = True
                 else:
                     cluster_updated = False
-
+            
+            # If cluster was not empty and also not different, do not make any change.
             else:
                 cluster_updated = False
 
-        # If cluster is empty, update it.
-        elif proxmox_cluster == None:
-            # Search for Proxmox Cluster using create.cluster() function
-            cluster_id = create.virtualization.cluster().id
-
-            # Use Cluster ID to update NODE information
-            netbox_node.cluster.id = cluster_id
-
-            if netbox_node.save() == True:
-                cluster_updated = True
-            else:
-                cluster_updated = False
-        
-        # If cluster was not empty and also not different, do not make any change.
-        else:
-            cluster_updated = False
-
-    except Exception as error:
-        print(f"[ERROR] {error}\n netbox_node: {netbox_node}\n> netbox_node.cluster.id: {netbox_node.cluster.id}\n> proxmox_cluster: {proxmox_cluster}")
-
-
+        except Exception as error:
+            print(f"[ERROR] {error}\n netbox_node: {netbox_node}\n> netbox_node.cluster.id: {netbox_node.cluster.id}\n> proxmox_cluster: {proxmox_cluster}")
+    else:
+        print(f"netbox_node: {netbox_node}")
+        cluster_updated = False
 
     return cluster_updated
 
