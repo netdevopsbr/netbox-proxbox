67cf78e976e6d079be32957fbea4b916829d0fc5
emersonfelipesp <emersonfelipe.2003@gmail.com>
2023-03-28 15:13:49 +0000

    Add HTML code to homepage of FastAPI

--- netbox_proxbox/main.py
+++ netbox_proxbox/main.py
@@ -1,12 +1,41 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from netbox_proxbox import proxbox_api
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World! dsdsasdad"}
+async def root(response_class=HTMLResponse):
+    html_content = """
+    <html>
+        <head>
+            <title>FastAPI | Home</title>
+        </head>
+        <body>
+            <div align=center>
+                <h1>FastAPI instance</h1>
+                <p>It is running so <a href="https://github.com/netdevopsbr/netbox-proxbox" target="_blank">Proxbox</a> can use fome of the useful features like:
+                    <br>
+                    <br>
+                        <strong>
+                            <a href="https://fastapi.tiangolo.com/advanced/websockets/?h=websocket" target="_blank">
+                                Websocket
+                            </a>
+                        </strong>
+                    <br>
+                        <strong>
+                            <a href="https://fastapi.tiangolo.com/async/" target="_blank">
+                                Assynchronous code (async / await)
+                            </a>
+                        </strong>
+                    
+                </p>
+            </div>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/full_update")
 async def full_update():
