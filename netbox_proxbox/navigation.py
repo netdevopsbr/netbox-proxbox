dd6768af3897e445e7117c7b0786f9887fb41eed
emersonfelipesp <emerson@netdevopsbr.com>
2023-07-20 14:20:26 +0000

    Move current navigation to top-level navigation

--- netbox_proxbox/navigation.py
+++ netbox_proxbox/navigation.py
@@ -1,25 +1,15 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem
+from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 from utilities.choices import ButtonColorChoices
 
-menu_items = (
-    PluginMenuItem(
-        link="plugins:netbox_proxbox:home",
-        link_text="Home",
-    ),
+proxbox_item = PluginMenuItem(
+    link='plugins:netbox_proxbox:home',
+    link_text='Full Update',
 )
 
-'''
-buttons=(
-    PluginMenuButton(
-        # match the names of the path for create view defined in ./urls.py
-        link="plugins:netbox_proxbox:proxmoxvm_add",
-        # text that appears when hovering the ubtton
-        title="Add",
-        # font-awesome icon to use
-        icon_class="mdi mdi-plus-thick", # 'fa fa-plus' didn't work
-        # defines color button to green
-        color=ButtonColorChoices.GREEN,
-        permissions=["netbox_proxbox.add_proxmoxvm"],
+menu = PluginMenu(
+    label='Proxbox',
+    groups=(
+        ('Proxmox Plugin', (proxbox_item,)),
     ),
-),
-'''
+    icon_class='mdi mdi-dns'
+)
