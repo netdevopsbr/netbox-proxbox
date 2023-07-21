beb16c882130b35808d0075cb8de87356eeb705e
emersonfelipesp <emerson@netdevopsbr.com>
2023-07-21 14:59:52 +0000

    Add ']' to fix array error or urls.py module

--- netbox_proxbox/urls.py
+++ netbox_proxbox/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
 
     # Proxbox API full update
     path("full_update/", views.ProxmoxFullUpdate.as_view(), name="proxmoxvm_full_update"),
-    path("single_update/", views.ProxmoxSingleUpdate.as_view(), name="proxmoxvm_single_update"),
+    path("single_update/", views.ProxmoxSingleUpdate.as_view(), name="proxmoxvm_single_update")
+]
