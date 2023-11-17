0a3144285155b9381f52822412bb4f6690237c3f
emersonfelipesp <emersonfelipe.2003@gmail.com>
2023-11-17 21:10:32 +0000

    Parent 'NetboxBase' class working with both 'ClusterType' and 'Sites' classes. Reduced a lot of code repetition.

--- netbox_proxbox/backend/routes/netbox/virtualization/__init__.py
+++ netbox_proxbox/backend/routes/netbox/virtualization/__init__.py
@@ -22,3 +22,19 @@ async def create_cluster_types(
     **data:** JSON to create the Cluster Type on Netbox. You can create any Cluster Type you want, like a proxy to Netbox API.
     """
     return await cluster_type.post(default, data)
+
+
+from netbox_proxbox.backend.session.netbox import NetboxSessionDep
+@router.get("/testing")
+async def get_testing(
+    nb: NetboxSessionDep,
+    app: str  | None = None,
+    endpoint: str | None = None,
+): 
+    response_list = []
+    
+    teste = getattr(getattr(nb.session, app), endpoint)
+    for item in teste.all():
+        response_list.append(item)
+
+    return response_list
