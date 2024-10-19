174ebb59f4b7c5885700f0e0de07415ce17b4a4c
Emerson Felipe <emersonfelipe.2003@gmail.com>
2024-10-19 20:05:14 +0000

    Add 'service buttons' on FastAPI card on Proxbox homepage and 'Proxbox fit-it' button

--- netbox_proxbox/urls.py
+++ netbox_proxbox/urls.py
@@ -13,10 +13,17 @@ urlpatterns = [
     path('contributing/', views.ContributingView.as_view(), name='contributing'),
     path('community/', views.CommunityView.as_view(), name='community'),
     
+    path('fix-proxbox-backend/', views.FixProxboxBackendView.as_view(), name='fix-proxbox-backend'),
+    path('start-proxbox-backend/', views.FixProxboxBackendView.as_view(), name='start-proxbox-backend'),
+    path('stop-proxbox-backend/', views.StopProxboxBackendView.as_view(), name='stop-proxbox-backend'),
+    path('restart-proxbox-backend/', views.RestartProxboxBackendView.as_view(), name='restart-proxbox-backend'),
+    path('status-proxbox-backend/', views.StatusProxboxBackendView.as_view(), name='status-proxbox-backend'),
+
     # Redirect to: "https://github.com/orgs/netdevopsbr/discussions"
     path('discussions/', views.DiscussionsView, name='discussions'),
     path('discord/', views.DiscordView, name='discord'),
     path('telegram/', views.TelegramView, name='telegram'),
+    
     
     # Base Views
     # path("list/", views.ProxmoxVMListView.as_view(), name="proxmoxvm_list"),
