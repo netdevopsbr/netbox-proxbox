f38d52c313cd536b0e90b8881961f367dceb16ea
Emerson Felipe <emersonfelipe.2003@gmail.com>
2024-10-21 20:44:36 +0000

    Remove all unused Views from views.py

--- netbox_proxbox/urls.py
+++ netbox_proxbox/urls.py
@@ -24,19 +24,9 @@ urlpatterns = [
     path('discord/', views.DiscordView, name='discord'),
     path('telegram/', views.TelegramView, name='telegram'),
     
-    
-    # Base Views
-    # path("list/", views.ProxmoxVMListView.as_view(), name="proxmoxvm_list"),
-    # <int:pk> = plugins/netbox_proxmoxvm/<pk> | example: plugins/netbox_proxmoxvm/1/
-    # ProxmoxVMView.as_view() - as.view() is need so that our view class can process requests.
-    # as_view() takes request and returns well-formed response, that is a class based view.
-    # path("<int:pk>/", views.ProxmoxVMView.as_view(), name="proxmoxvm"),
-    # path("add/", views.ProxmoxVMCreateView.as_view(), name="proxmoxvm_add"),
-    # path("<int:pk>/delete/", views.ProxmoxVMDeleteView.as_view(), name="proxmoxvm_delete"),
-    # path("<int:pk>/edit/", views.ProxmoxVMEditView.as_view(), name="proxmoxvm_edit"),
 
     # Proxbox API full update
     path("full_update/", views.ProxmoxFullUpdate.as_view(), name="proxmoxvm_full_update"),
-    path("single_update/", views.ProxmoxSingleUpdate.as_view(), name="proxmoxvm_single_update"),
-    path("proxmox/cluster/", views.ProxmoxCluster.as_view(), name="proxmox_cluster"),
+    #path("single_update/", views.ProxmoxSingleUpdate.as_view(), name="proxmoxvm_single_update"),
+    #path("proxmox/cluster/", views.ProxmoxCluster.as_view(), name="proxmox_cluster"),
 ]
